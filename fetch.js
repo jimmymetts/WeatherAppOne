7937ed97f45a05d5a2770409e3d845bc39e612e6
jimmymetts <jimmymetts@bellsouth.net>
2021-04-11 10:08:36 -0500

    first commit

---
+++ fetch.js
@@ -0,0 +1,23 @@
+class Fetch {
+    async getCurrent(input) {
+      const myKey = "8f307892df318217ca3702745cf00c43";
+  
+      //make request to url
+  
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+      );
+
+      
+/*          const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q={input},{input}&appid={myKey}`
+          ); */
+     
+  
+      const data = await response.json();
+  
+      console.log(data);
+  
+      return data;
+    }
+  }
